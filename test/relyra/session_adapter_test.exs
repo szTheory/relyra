0daf0272a2562278d566df93e53146175e942fe3
szTheory <szTheory@users.noreply.github.com>
2026-05-07 20:23:25 -0400

    test(24-01): add failing test for session revocation

---
+++ test/relyra/session_adapter_test.exs
@@ -0,0 +1,30 @@
+defmodule Relyra.SessionAdapterTest do
+  use ExUnit.Case, async: true
+
+  alias Relyra.SessionAdapter
+  alias Relyra.Error
+
+  defmodule TestAdapter do
+    @behaviour Relyra.SessionAdapter
+
+    def establish_session(_subject, _context, _opts) do
+      {:ok, %{established: true}}
+    end
+
+    def revoke_session(_subject, session_index, _context, _opts) do
+      {:ok, %{revoked: session_index}}
+    end
+  end
+
+  describe "revoke_session/4" do
+    test "returns error when no adapter is configured" do
+      assert {:error, %Error{type: :adapter_not_configured}} =
+               SessionAdapter.revoke_session(%{}, "session_123", %{})
+    end
+
+    test "dispatches to configured adapter" do
+      assert {:ok, %{revoked: "session_123"}} =
+               SessionAdapter.revoke_session(%{}, "session_123", %{}, session_adapter: TestAdapter)
+    end
+  end
+end
