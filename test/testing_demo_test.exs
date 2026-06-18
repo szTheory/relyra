6bd558f7d02f9c65d3b7002c27501347703f3b42
szTheory <szTheory@users.noreply.github.com>
2026-06-18 14:39:44 -0400

    test(65-01): isolate test_support_demo_test.exs and create testing_demo_test.exs

---
+++ test/testing_demo_test.exs
@@ -0,0 +1,36 @@
+defmodule Relyra.TestingDemoRouter do
+  use Phoenix.Router
+
+  post("/:connection_id/acs", Relyra.TestingDemoController, :acs)
+end
+
+defmodule Relyra.TestingDemoController do
+  use Phoenix.Controller, formats: [html: "Phoenix.HTML"]
+
+  def acs(conn, _params) do
+    conn
+    |> Plug.Conn.assign(:current_user, %{email: "alice@example.com"})
+    |> Phoenix.Controller.text("ok")
+  end
+end
+
+defmodule Relyra.TestingDemoTest do
+  use ExUnit.Case, async: false
+
+  @endpoint Relyra.TestingDemoRouter
+
+  test "adopters can write a tiny integration test" do
+    conn = Phoenix.ConnTest.build_conn()
+    fixture = Relyra.Testing.signed_success(name_id: "alice@example.com")
+
+    conn =
+      Relyra.Testing.Phoenix.post_response(
+        conn,
+        @endpoint,
+        "/#{fixture.connection.id}/acs",
+        fixture
+      )
+
+    assert %Plug.Conn{assigns: %{current_user: %{email: "alice@example.com"}}} = conn
+  end
+end
